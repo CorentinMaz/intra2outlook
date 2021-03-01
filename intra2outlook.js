7c86e1cc069f3163c41275449381f5c190f01235
Clement Madzar <cmadzar@localhost.localdomain>
2021-03-01 11:42:49 +0100

    print data-> json format

--- intra2outlook.js
+++ intra2outlook.js
@@ -3,11 +3,5 @@ const fetch = require("node-fetch");
 let url = 'https://intra.epitech.eu/auth-6f31b8a817164044ace66cae8a6672fa91bca461/planning/load?format=json'
 
 fetch(url)
-    .then(response => {
-        if (response.ok) {
-            console.log('SUCCESS')
-        } else
-            console.log('not SUCCESSFUL')
-        response.json()
-    })
-    .then(data => console.log(data))
+    .then(response => response.json())
+    .then(data => console.log(data));
