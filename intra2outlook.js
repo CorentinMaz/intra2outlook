83cad6ff54cb1210e894d5524b13721e3cae0f3f
corentin mazabrard <cmazabrard@c.mazabrard>
2021-03-01 14:43:08 +0100

    axios get

--- intra2outlook.js
+++ intra2outlook.js
@@ -1,7 +1,18 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
+// let url = 'https://intra.epitech.eu/auth-6f31b8a817164044ace66cae8a6672fa91bca461/planning/load?format=json'
+// // const test = fetch(url) .then(response => response.json()) .then(data => data);
 
-let url = 'https://intra.epitech.eu/auth-6f31b8a817164044ace66cae8a6672fa91bca461/planning/load?format=json'
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => data);
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data));
+// console.log(test);
+
+const request = require("axios");
+
+request.get('https://intra.epitech.eu/auth-6f31b8a817164044ace66cae8a6672fa91bca461/planning/load?format=json')
+    .then(res => {
+        const data = res.data;
+        // console.log(data);
+        console.log(data.get('event_required:'));
+    })
