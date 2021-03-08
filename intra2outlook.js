435869cd5712b87cc3cdd2cabb22961e11a00366
Clement Madzar <cmadzar@localhost.localdomain>
2021-03-08 12:24:35 +0100

    get right informations about sessions

--- intra2outlook.js
+++ intra2outlook.js
@@ -1,12 +1,27 @@
 const request = require("axios");
 const url = 'https://intra.epitech.eu/auth-6f31b8a817164044ace66cae8a6672fa91bca461/planning/load?format=json';
 
+var sessions = [];
+
 function prinata(data) {
     const obj = data;
     for (var i = 0; i < obj.length; i++) {
-        if (obj[i].event_registered == 'registered')
-            console.log(obj[i]);
+        if (obj[i].event_registered == 'registered') {
+            var session = [];
+            session.push((obj[i]).scolaryear);
+            session.push((obj[i]).titlemodule);
+            session.push((obj[i]).acti_title);
+            session.push((obj[i]).start);
+            session.push((obj[i]).end);
+            session.push((obj[i]).type_title);
+            if (obj[i].room.code == undefined)
+                session.push((obj[i]).room.type);
+            else
+                session.push((obj[i]).room.code);
+            sessions.push(session);
+        }
     }
+    console.log(sessions);
 }
 
 request.get(url)
