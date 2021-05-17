fc98b7575892679603e3b9bdeab28c087ca96b95
Corentin <corentin.mazabrard@epitech.eu>
2021-05-17 14:01:03 +0200

    add send mail with .ics

--- create_file.py
+++ create_file.py
@@ -1,6 +1,47 @@
 #!/usr/bin/env python3
 import os
+import email
+import smtplib
+import ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+def send_mail(receiver_email):
+    subject = "Planning intra"
+    body = ""
+    sender_email = "intra2outlook@gmail.com"
+    password = "N2Cn8Pa4q"
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email
+
+    message.attach(MIMEText(body, "plain"))
+
+    filename = "intra2outlook.ics"
+
+    with open(filename, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    encoders.encode_base64(part)
+
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+
+    message.attach(part)
+    text = message.as_string()
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
 
 def write_mail(fichier, title, start, end, location, description):
     fichier.write("BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN\nBEGIN:VEVENT\n")
@@ -12,12 +53,12 @@ def write_mail(fichier, title, start, end, location, description):
     fichier.write("STATUS:CONFIRMED\nSEQUENCE:3\nBEGIN:VALARM\nTRIGGER:-PT10M\nDESCRIPTION:Pickup Reminder\nACTION:DISPLAY\nEND:VALARM\nEND:VEVENT\nEND:VCALENDAR")
 
 def main():
-    print ("Creating")
     fichier = open("intra2outlook.ics", "w+")
     fichier = open("intra2outlook.ics", "a")
     write_mail(fichier, "title", "20210517T103400", "20210517T110400", "Salle machine 1", "description")
-    # os.remove("mail.ics")
     fichier.close()
+    send_mail("thomas1.grand@epitech.eu")
+    os.remove("intra2outlook.ics")
 
 if __name__ == "__main__":
     main()
